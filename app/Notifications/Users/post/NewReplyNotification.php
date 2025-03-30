9b26e85ae835f27942e7064d90e4cc2eab7b7614
Laith Abushanab <laith99shanab@gmail.com>
2025-03-30 21:08:54 +0300

    up

--- app/Notifications/Users/post/NewReplyNotification.php
+++ app/Notifications/Users/post/NewReplyNotification.php
@@ -12,13 +12,17 @@ class NewReplyNotification extends Notification
     use Queueable;
 
     public $user;
+    public $commentId;
+    public $postId;
 
     /**
      * Create a new notification instance.
      */
-    public function __construct($user)
+    public function __construct($user, $commentId, $postId)
     {
         $this->user = $user;
+        $this->commentId = $commentId;
+        $this->postId = $postId;
     }
 
     /**
@@ -41,8 +45,14 @@ class NewReplyNotification extends Notification
         return [
             "title_en" => "There is a new reply in your comment",
             "title_ar" => "يوجد تعليق جديد",
-            "body_en" => "The User " . $this->user->name . " has add new reply in your comment",
-            "body_ar" => "تم إنشاء رد جديد على تعليقك بواسطة المستخدم " . $this->user->name
+            "body_en" => "The User " . $this->user->username . " has add new reply in your comment",
+            "body_ar" => "تم إنشاء رد جديد على تعليقك بواسطة المستخدم " . $this->user->username,
+            'options' => [
+                'type'       => 'comment',
+                'slug'       => null,
+                'post_id'    => $this->postId,
+                'comment_id' => $this->commentId,
+            ]
         ];
     }
 }

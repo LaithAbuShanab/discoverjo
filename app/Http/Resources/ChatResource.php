86f7be9cf114269e334eb14bdcae79129b9fd5d5
Laith Abushanab <laith99shanab@gmail.com>
2025-05-03 12:11:57 +0300

    fixed get chat

--- app/Http/Resources/ChatResource.php
+++ app/Http/Resources/ChatResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class ChatResource extends JsonResource
 {
@@ -19,7 +20,7 @@ class ChatResource extends JsonResource
             'id'                => $this->id,
             'user_id'           => $this->user->id,
             'username'          => $this->user->username,
-            'username_for_me'   => __('app.you'),
+            'username_for_me'   => $this->user->id == Auth::guard('api')->user()->id ? __('app.you') : $this->user->username,
             'user_image'        => $this->user->getFirstMediaUrl('avatar', 'avatar_app'),
             'message'           => $this->message_txt,
             'message_file'      => $this->getFirstMediaUrl('file', 'file_thumb'),

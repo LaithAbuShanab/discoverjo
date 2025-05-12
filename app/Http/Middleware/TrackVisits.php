c8f45b317a1c8d32edea46060debaba12df68913
Laith Abushanab <laith99shanab@gmail.com>
2025-05-12 18:03:33 +0300

    cache TrackVisits

--- app/Http/Middleware/TrackVisits.php
+++ app/Http/Middleware/TrackVisits.php
@@ -3,9 +3,9 @@
 namespace App\Http\Middleware;
 
 use App\Models\Visit;
-use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Symfony\Component\HttpFoundation\Response;
 
 class TrackVisits
@@ -19,14 +19,12 @@ class TrackVisits
     {
         $userId = auth()->guard('api')->user()->id ?? null;
         $ip = $request->ip();
-        $today = Carbon::today();
 
-        $alreadyVisited = Visit::whereDate('created_at', $today)
-            ->when($userId, fn($q) => $q->where('user_id', $userId))
-            ->when(!$userId, fn($q) => $q->where('ip_address', $ip))
-            ->exists();
+        $cacheKey = 'visit_' . ($userId ?? $ip);
 
-        if (! $alreadyVisited) {
+        $notVisitedRecently = Cache::add($cacheKey, true, now()->addMinutes(1));
+
+        if ($notVisitedRecently) {
             Visit::create([
                 'user_id' => $userId,
                 'ip_address' => $ip,

3bb89776e7a943d4a5a4d9015abeff18b3a385ce
Asma Abughaith <asma.abughaith@gmail.com>
2025-05-17 15:11:05 +0300

    edit guide trip table

--- database/migrations/2025_05_17_142744_add_fulltext_index_to_guide_trips_table.php
+++ database/migrations/2025_05_17_142744_add_fulltext_index_to_guide_trips_table.php
@@ -17,15 +17,12 @@ return new class extends Migration
         DB::statement("
             ALTER TABLE guide_trips
             ADD COLUMN name_en TEXT GENERATED ALWAYS AS (json_unquote(json_extract(name, '$.en'))) STORED,
-            ADD COLUMN name_ar TEXT GENERATED ALWAYS AS (json_unquote(json_extract(name, '$.ar'))) STORED,
-            ADD COLUMN description_en TEXT GENERATED ALWAYS AS (json_unquote(json_extract(description, '$.en'))) STORED,
-            ADD COLUMN description_ar TEXT GENERATED ALWAYS AS (json_unquote(json_extract(description, '$.ar'))) STORED
+            ADD COLUMN name_ar TEXT GENERATED ALWAYS AS (json_unquote(json_extract(name, '$.ar'))) STORED
         ");
 
         // إضافة فهرس FULLTEXT عبر Blueprint
         Schema::table('guide_trips', function (Blueprint $table) {
             $table->fullText(['name_en', 'name_ar'], 'guide_trips_fulltext_name');
-            $table->fullText(['description_en', 'description_ar'], 'guide_trips_fulltext_description');
         });
     }
 
@@ -37,16 +34,13 @@ return new class extends Migration
         // حذف الفهارس أولًا
         Schema::table('guide_trips', function (Blueprint $table) {
             $table->dropFullText('guide_trips_fulltext_name');
-            $table->dropFullText('guide_trips_fulltext_description');
         });
 
         // ثم حذف الأعمدة المشتقة
         DB::statement("
             ALTER TABLE guide_trips
             DROP COLUMN name_en,
-            DROP COLUMN name_ar,
-            DROP COLUMN description_en,
-            DROP COLUMN description_ar
+            DROP COLUMN name_ar
         ");
     }
 };

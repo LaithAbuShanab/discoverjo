bd31eaaf6627588aa4e778f5123dc7e1fae005d3
Laith Abushanab <laith99shanab@gmail.com>
2025-05-17 19:50:23 +0300

    Fix Logs name

--- app/Filament/Pages/CustomLogs.php
+++ app/Filament/Pages/CustomLogs.php
@@ -4,6 +4,7 @@ namespace App\Filament\Pages;
 
 use FilipFonal\FilamentLogManager\Pages\Logs;
 use Symfony\Component\Finder\SplFileInfo;
+
 class CustomLogs extends Logs
 {
     public function getFileNames($files): \Illuminate\Support\Collection
@@ -13,5 +14,4 @@ class CustomLogs extends Logs
             return [$filename => $filename];
         });
     }
-
 }

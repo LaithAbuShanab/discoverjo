940a5172bfaf926295d9c60ab205e9de0f7b1d66
Asma Abughaith <asma.abughaith@gmail.com>
2025-05-17 11:48:46 +0300

    add cors to project

---
+++ config/cors.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
+
+    'allowed_origins' =>['https://admin.discoverjo.com','https://test.discoverjo.com','https://discoverjo.com'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => [
+        'Authorization',
+        'Cache-Control',
+        'Content-Length',
+        'User-Agent',
+        'Accept-Encoding',
+        'Connection',
+        'Content-Type',
+        'X-Requested-With',
+        'Content-Language',
+        'X-API-KEY',
+        'Accept',
+        'Host'
+    ],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];

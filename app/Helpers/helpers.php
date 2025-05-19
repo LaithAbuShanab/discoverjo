0ee59b8969b90bbfec6c01a81efec48be75882bb
Asma Abughaith <asma.abughaith@gmail.com>
2025-05-19 09:46:42 +0300

    improve set location api

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -252,6 +252,12 @@ function handleWarning(object $record): void
 
 function getAddressFromCoordinates(float $lat, float $lng, string $language): string
 {
+    $user = Auth::guard('api')->user();
+    if ($user->latitude && $user->longitude) {
+        if (isWithinRadius($user->latitude, $user->longitude, $lat, $lng)) {
+            return "{$user->getTranslation('address', $language)}";
+        }
+    }
     // Validate input coordinates
     if (!is_numeric($lat) || !is_numeric($lng) || $lat < -90 || $lat > 90 || $lng < -180 || $lng > 180) {
         return 'Invalid Coordinates';
@@ -309,6 +315,19 @@ function getAddressFromCoordinates(float $lat, float $lng, string $language): st
 }
 
 
+
+function isWithinRadius($lat1, $lon1, $lat2, $lon2, $radiusMeters = 500): bool {
+    $earthRadius = 6371000; // meters
+    $dLat = deg2rad($lat2 - $lat1);
+    $dLon = deg2rad($lon2 - $lon1);
+    $a = sin($dLat/2) * sin($dLat/2) +
+        cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
+        sin($dLon/2) * sin($dLon/2);
+    $c = 2 * atan2(sqrt($a), sqrt(1-$a));
+    $distance = $earthRadius * $c;
+
+    return $distance <= $radiusMeters;
+}
 function getWeatherNow($latitude, $longitude)
 {
     $jordanTime = Carbon::now('Asia/Riyadh');

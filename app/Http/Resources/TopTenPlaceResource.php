70a3d1f4707d3c7b9ffdec5e765e93feb6fd714c
Asma Abughaith <asma.abughaith@gmail.com>
2025-05-22 19:24:44 +0300

    use origin image for top ten

--- app/Http/Resources/TopTenPlaceResource.php
+++ app/Http/Resources/TopTenPlaceResource.php
@@ -30,7 +30,7 @@ class TopTenPlaceResource extends JsonResource
                 'place_slug'=>$place->place->slug,
                 'name' => $place->place->name,
                 'description'=>$place->place->description,
-                'image' => $place->place->getFirstMediaUrl('main_place','main_place_app'),
+                'image' => $place->place->getFirstMediaUrl('main_place'),
                 'region' => $place->place->region->name,
                 'address' => $place->place->address,
                 'rating' => $place->place->rating,

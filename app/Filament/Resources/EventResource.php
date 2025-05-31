77e12f68f3ec35e821fc4e016a0d93dad37ca8d9
Asma Abughaith <asma.abughaith@gmail.com>
2025-05-31 14:03:45 +0300

    remove requeried form price and attendance number in event

--- app/Filament/Resources/EventResource.php
+++ app/Filament/Resources/EventResource.php
@@ -86,8 +86,8 @@ class EventResource extends Resource
                             ]),
                         Grid::make(2)
                             ->schema([
-                                Forms\Components\TextInput::make('price')->placeholder('Please Enter Price')->numeric(),
-                                Forms\Components\TextInput::make('attendance_number')->placeholder('Please Enter Attendance Number')->numeric(),
+                                Forms\Components\TextInput::make('price')->placeholder('Please Enter Price')->nullable()->numeric(),
+                                Forms\Components\TextInput::make('attendance_number')->placeholder('Please Enter Attendance Number')->nullable()->numeric(),
                             ]),
                         Grid::make(2)
                             ->schema([

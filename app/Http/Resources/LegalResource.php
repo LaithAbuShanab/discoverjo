6a1c16e41b1a9bfbc441a7df91f05fb2dd42a70e
Asma Abughaith <asma.abughaith@gmail.com>
2025-06-13 17:53:35 +0300

    add last update in legal document

--- app/Http/Resources/LegalResource.php
+++ app/Http/Resources/LegalResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\LegalDocument;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,11 +15,13 @@ class LegalResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $lastLegal = LegalDocument::latest('updated_at')->first()?->updated_at;
         $terms = [];
         foreach ( $this->terms as $term) {
             $terms[] = $term->content;
         }
         return [
+            'last_update'=>$lastLegal,
             'title' => $this->title,
             'content' =>  $this->content,
             'terms'=>$terms

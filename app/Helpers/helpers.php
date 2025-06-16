f36027c1aad545b7377d0c3d656874cb215cc7d9
Asma Abughaith <asma.abughaith@gmail.com>
2025-06-16 17:43:35 +0300

    add dd in notifications

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -103,7 +103,6 @@ function sendNotification($deviceTokens, $data)
     $dataPayload['icon'] = $data['notification']['image'] ?? asset('default-icon.png');
     $dataPayload['sound'] = $data['notification']['sound'] ?? 'default';
 
-    dd($dataPayload);
     $message = CloudMessage::new()
         ->withNotification($notification)
         ->withData($dataPayload);
